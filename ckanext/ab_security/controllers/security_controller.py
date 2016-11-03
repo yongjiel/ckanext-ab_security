4118b389060b841ded003e9b09ea893afe9d70fd
Yongjie Liang <jack.liang@gov.ab.ca>
2016-11-03 11:53:48 -0600

    This version is able to work alone now with ckan core.

---
+++ ckanext/ab_security/controllers/security_controller.py
@@ -0,0 +1,106 @@
+import ckan.lib.base as base
+from ckan.common import _, request, c
+import ckan.lib.helpers as helpers
+import ckan.model as model
+import ckan.plugins.toolkit as toolkit
+
+
+class SecurityClassificationController(base.BaseController):
+    """ This controller controls the actions of adding and removing
+	    users for specific security classification. It is implemented
+	    in organization edit function.
+    """ 
+    def _get_context_controller(self):
+        context = {'model': model, 'session': model.Session,
+                   'user': toolkit.c.user or toolkit.c.author}
+        try:
+            toolkit.check_access('member_create', context, {})
+        except toolkit.NotAuthorized:
+            toolkit.abort(401, toolkit._('User not authorized to manage page'))
+
+        controller = 'ckanext.ab_security.controllers:SecurityClassificationController'
+        return context, controller
+
+    
+    def _action(self, action, context, username, groupname, 
+                dataset_type, classification, success_msg):
+        try:
+            toolkit.get_action(action)(context,
+                            data_dict={'username': username, 
+                                       'groupname': groupname, 
+                                       'dataset_type': dataset_type,
+                                       'classification': classification}
+            )
+        except toolkit.NotAuthorized:
+            toolkit.abort(401,
+                          toolkit._('Unauthorized to perform that action'))
+        except toolkit.ObjectNotFound as e :
+            error_message = (e.message or e.error_summary
+                         or e.error_dict)
+            helpers.flash_error(error_message)
+        except toolkit.ValidationError as e:
+            error_message = e.error_dict
+            helpers.flash_error(error_message['message'])
+        else:
+            helpers.flash_success(toolkit._(success_msg))
+
+
+    def manage(self, id):
+        context, controller = self._get_context_controller()
+        username = toolkit.request.params.get('username')
+        dataset_type = toolkit.request.params.get("field-dataset_type")
+        classification = toolkit.request.params.get('field-classification')
+        group_object = model.Group.get(id)
+        groupname = group_object.name
+
+        toolkit.c.group_dict = group_object
+
+        if toolkit.request.method == 'POST':
+            if username:
+                self._action('security_member_create', 
+                             context, 
+                             username, 
+                             groupname, 
+                             dataset_type,
+                             classification,
+                             "User \"{0}\" is now an Member of Security".format(username))
+            else:
+                helpers.flash_error("Please input username first.")
+            return toolkit.redirect_to(toolkit.url_for(controller=controller,
+                                                       action='manage', 
+                                                       id=id))
+
+        security_members_list = toolkit.get_action('security_member_list')(context, 
+                                                    data_dict={'groupname': groupname})
+        return toolkit.render(
+            'organization/manage_security_members.html',
+            extra_vars={
+                'security_members_list': security_members_list,
+            }
+        )
+
+
+    def remove(self, id):
+        if 'cancel' in toolkit.request.params:
+            toolkit.redirect_to(controller=controller, action='manage', id=id)
+
+        context, controller = self._get_context_controller()
+        user_id = toolkit.request.params['user']
+        user_object = model.User.get(user_id)
+        username = user_object.name
+        group_object = model.Group.get(id)
+        groupname = group_object.name
+        dataset_type = toolkit.request.params['dataset_type']
+        classification = toolkit.request.params['classification']
+        
+        if toolkit.request.method == 'POST' and user_id:
+            self._action('security_member_delete', 
+                         context, 
+                         username, 
+                         groupname, 
+                         dataset_type,
+                         classification,
+                         "User \"{0}\" is no longer an Member of Security".format(username))
+
+        return toolkit.redirect_to(
+                helpers.url_for(controller=controller, action='manage', id=id))
--- ckanext/ab_security/plugin.py
+++ ckanext/ab_security/plugin.py
@@ -1,13 +1,96 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.ab_security.helpers as helpers
+from model.security_member_model import (
+    get_member_classification, 
+    add_member_classification
+)
+from model import security_member_model
+from ckan.config.routing import SubMapper
+from ckanext.ab_security.logic import action
 
 
 class Ab_SecurityPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
-    # IConfigurer
-
+    """  
+    IConfigurer
+    """
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ab_security')
+
+
+    """
+    ITemplateHelpers
+    """
+    def get_helpers(self):
+        return {
+            'ab_security_is_admin': helpers.is_admin,
+            'ab_security_get_all_classifications': helpers.get_all_classifications,
+            'ab_security_get_dataset_types_w_classification': helpers.get_dataset_types_w_classification,
+            'ab_security_get_org': helpers.get_org,
+            'ab_security_get_current_user_id': helpers.get_current_user_id,
+            'ab_security_get_classification': helpers.get_classification
+        }
+
+    """
+    IConfigurable
+    """
+    def configure(self, config):
+        security_member_model.setup()
+
+
+    """
+    IRoutes
+    """
+    def before_map(self, map):
+        controller = 'ckanext.ab_security.controllers:SecurityClassificationController'
+        with SubMapper(map, controller=controller) as m:
+            m.connect('security_members', '/organization/{id}/security_members',
+                      action='manage', ckan_icon='user')
+            m.connect('security_members_remove', '/organization/{id}/security_members_remove',
+                      action='remove')
+
+        return map
+
+
+    
+    """
+    IActions
+    """
+    def get_actions(self):
+        actions = dict((name, function) for name, function
+                       in action.__dict__.items()
+                       if callable(function))
+        return actions
+
+
+    """
+    IPackageController
+    """
+    def after_show(self, context, pkg_dict):
+    	""" check if the resources classification is in user's assigned classification 
+            range and filter out the resources needed.
+    	"""
+        current_user_id = helpers.get_current_user_id()
+    	classification = helpers.get_classification(current_user_id, 
+    		                                        pkg_dict['owner_org'], 
+    		                                        pkg_dict['type'])
+        if not classification:
+            classification = 1
+        resources = []
+        for res in pkg_dict['resources']:
+        	if res:
+        		classif = res.get('classification')
+        		if classif and classif <= classification or not classif:
+        			resources.append(res)
+        pkg_dict['resources'] = resources
+        return pkg_dict
+        
